9ee4f4dadc5465a6f7f4cff021352b3271db605b
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-04-04 15:53:55 -0400

    Active Nav Select Fix
    - Back to displaying the correct active button in the navbar

--- src/nav.js
+++ src/nav.js
@@ -8,13 +8,13 @@ activeNavButton_el.addEventListener('click', () => {
     if (simNavButton_el.classList.contains('active')){
         simNavButton_el.classList.remove('active');
     }
-    realNavButton_el.classList.add('active');
+    activeNavButton_el.classList.add('active');
 });
 
 simNavButton_el.addEventListener('click', () => {
     selectNavText_el.textContent = 'Simulated';
-    if (realNavButton_el.classList.contains('active')){
-        realNavButton_el.classList.remove('active');
+    if (activeNavButton_el.classList.contains('active')){
+        activeNavButton_el.classList.remove('active');
     }
     simNavButton_el.classList.add('active');
 });
