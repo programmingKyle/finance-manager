4238951b84fc831411c7a8423cfdd1ef6ad70720
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-04-04 16:19:19 -0400

    Open add Project Overlay
    - Click on the plus button and it will open an overlay that will later have functionality

---
+++ src/projectHandler.js
@@ -0,0 +1,12 @@
+const addProjectButton_el = document.getElementById('addProjectButton');
+
+addProjectButton_el.addEventListener('click', () => {
+    overlayContainer_el.style.display = 'flex';
+    loadOverlayContent('overlays.html', '#addProjectContainer', addProjectListeners);
+});
+
+function addProjectListeners(){
+    const addProjectCloseButton_el = document.getElementById('addProjectCloseButton');
+    const projectNameInput_el = document.getElementById('projectNameInput');
+    const confirmAddProjectButton_el = document.getElementById('confirmAddProjectButton');
+}
